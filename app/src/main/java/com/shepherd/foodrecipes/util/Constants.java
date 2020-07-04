1e9fa16756f940844cccf3ae025d9665400a4ec6
Shepherd Siduli <shepherdsiduli@gmail.com>
2020-07-04 23:38:20 +0200

    Initial commit

---
+++ app/src/main/java/com/shepherd/foodrecipes/util/Constants.java
@@ -0,0 +1,8 @@
+package com.shepherd.foodrecipes.util;
+
+public class Constants {
+
+    public static final String BASE_URL = "https://recipesapi.herokuapp.com";
+
+    public static final String API_KEY = "";
+}
